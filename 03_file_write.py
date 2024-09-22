d7005b7d076c66e714a9432a9278a9c7c98ee796
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2024-09-22 20:52:36 +0500

    Update 03_file_write.py

---
+++ 03_file_write.py
@@ -0,0 +1,18 @@
+f = open("file.text")
+
+#lines = f.readlines()
+#print(lines, type(lines))
+
+line1 = f.readlines()
+print(line1, type(line1))
+
+line2 = f.readlines()
+print(line2, type(line2))
+
+line3 = f.readlines()
+print(line3, type(line3))
+
+line4 = f.readlines()
+print(line4, type(line4))
+
+f.close()
